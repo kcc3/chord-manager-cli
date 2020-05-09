ecf4103deeec4931a494dad880042f6896ad8beb
Kevin Chang <kevin.chaohwa.chang@gmail.com>
2020-05-08 20:08:27 -0700

    setup
    Setting up setup.py and requirements, getting initial structure of the cli set up for the first script function - tranposing chords

---
+++ setup.py
@@ -0,0 +1,32 @@
+import sys
+from setuptools import setup
+
+if sys.version_info[0] < 3:
+    with open('README.md') as f:
+        long_description = f.read()
+else:
+    with open('README.md', encoding='utf-8') as f:
+        long_description = f.read()
+
+setup(
+    name="chord_manager_cli",
+    version="0.1",
+
+    author="Kevin Chang",
+    author_email="kevin.chaohwa.chang@gmail.com",
+
+    description="command line tools for simplifying music chords",
+    long_description=long_description,
+
+    packages=["chord_manager_cli"],
+    install_requires=[
+        "pychord",
+        "click"
+    ],
+
+    entry_points={
+        "console_scripts": [
+            "transpose = chord_manager_cli.cli:cli"
+        ]
+    },
+)
